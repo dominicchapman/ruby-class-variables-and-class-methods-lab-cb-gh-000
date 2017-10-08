0a0d95d0964594bb01509ae42086cce68735d354
Dominic Chapman <dominicchapman@me.com>
2017-10-08 17:23:29 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,4 +29,15 @@ class Song
     @@genres
   end
 
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre_count] += 1
+      else
+        genre_count[genre_count] = 1
+      end
+    end
+  end
+
 end
