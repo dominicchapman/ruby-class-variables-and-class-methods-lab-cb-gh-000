b36e2e95195028dd45447135174b1ed70da14986
Dominic Chapman <dominicchapman@me.com>
2017-10-08 17:14:26 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,32 @@
+
+class Song
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  attr_reader :name, :artist, :genre
+
+  def initialize (name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+
+    @@count += 1
+    @@artists << artist unless @@artists.find(artist)
+  end
+
+  def count
+    @@count
+  end
+
+  def artists
+    @@artists
+  end
+
+  def genres
+    @@genres
+  end
+
+
+end
