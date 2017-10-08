4c6825a216026ee897b31cd4445616111a5eea18
Dominic Chapman <dominicchapman@me.com>
2017-10-08 17:29:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,11 @@
 
 class Song
 
+  attr_accessor :name, :artist, :genre
+
   @@count = 0
   @@artists = []
   @@genres = []
-
-  attr_accessor :name, :artist, :genre
 
   def initialize(name, artist, genre)
     @name = name
@@ -25,6 +25,18 @@ class Song
     @@artists
   end
 
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
+
   def self.genres
     @@genres
   end
@@ -41,16 +53,5 @@ class Song
     genre_count
   end
 
-  def self.artist_count
-    artist_count = {}
-    @@artists.each do |artist|
-      if artist_count[artist]
-        artist_count[artist] += 1
-      else
-        artist_count[artist] = 1
-      end
-    end
-    artist_count
-  end
 
 end
